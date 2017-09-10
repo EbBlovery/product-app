6f79770114ae7abcda0736f56a81edcaa795142d
EbBlovery <18217052735@163.com>
2017-09-10 22:48:10 +0800

    validate

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { ShareService } from '../share/share.service';
 
@@ -11,13 +12,33 @@ import { ShareService } from '../share/share.service';
 export class RegisterComponent implements OnInit {
   formModule:FormGroup = new FormGroup({
   	 username: new FormControl(''),
-  	 password: new FormControl('')
+  	 password: new FormControl(''),
+  	 pconfig: new FormControl('')
   });
-  constructor(private getService:ShareService) { }
+
+  private isSecrectTrue:boolean = false;
+  private isAdminTrue:boolean = false;
+
+  constructor(
+  	private getService:ShareService,
+  	private router:Router
+  ) { }
 
   ngOnInit() {
+
   }
   onSubmit(){
-      this.getService.getRegister(this.formModule.value).subscribe(data=> console.log(data));
+      this.getService.getRegister(this.formModule.value).subscribe(data=> this.isRouterLogin(data));
+  }
+  isRouterLogin(data:any){
+      if(data.code === 0){
+         this.router.navigate(['login']);
+      }else if(data.code === 1){
+         this.isSecrectTrue = true;
+      }else if(data.code === 2){
+         this.isAdminTrue = true;
+      }else{
+      	 alert('error');
+      }
   }
 }
